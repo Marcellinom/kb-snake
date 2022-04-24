aa4458bde44d4efcc963f1810f6141be46447729
Fathur45 <72728516+Fathur45@users.noreply.github.com>
2022-04-25 02:32:48 +0700

    Comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# importing libraries
+# import library
 from turtle import distance
 import pygame
 import time
@@ -7,13 +7,12 @@ from math import sqrt
 import sys
 from pygame.locals import *
 
-snake_speed = 20
-
-# Window size
+# Pembuatan dan Settingan Object Game
+# Ukuran Window
 window_x = 400
 window_y = 400
 
-# defining colors
+# RGB
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
@@ -21,41 +20,37 @@ green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 purple = pygame.Color(128, 0, 128)
 
-# Initialising pygame
+# Init pygame
 pygame.init()
 
-# Initialise game window
+# Setting dari Window
 pygame.display.set_caption('AI.Snake')
 game_window = pygame.display.set_mode((window_x, window_y))
 
-# FPS (frames per second) controller
+# FPS controller
 fps = pygame.time.Clock()
 
-# defining snake default position
+# Posisi awal snake
+snake_speed = 20
 snake_position = [100, 50]
 
-# defining first 4 blocks of snake
-# body
+# Posisi 4 block ular
 snake_body = [
     [100, 50],
     [90, 50],
     [80, 50],
     [70, 50]]
 
-# fruit position
+# Posisi fruit
 fruit_position = [random.randrange(1, (window_x // 10)) * 10,
                   random.randrange(1, (window_y // 10)) * 10]
 fruit_spawn = True
 
-# setting default snake direction
-# towards right
+# setting arah gerak awal snake
 direction = 'RIGHT'
 change_to = direction
 
-# initial score
-score = 0
-
-
+# Arah berlawanan
 opposite_direction = {
     'UP': 'DOWN',
     'DOWN': 'UP',
@@ -63,6 +58,7 @@ opposite_direction = {
     'RIGHT': 'LEFT'
 }
 
+# Arah yang mungkin untuk setiap arah
 secondary_move = {
     'UP': ('LEFT', 'RIGHT'),
     'DOWN': ('LEFT', 'RIGHT'),
@@ -70,45 +66,48 @@ secondary_move = {
     'RIGHT': ('UP', 'DOWN')
 }
 
-# displaying Score function
 
 
+# Function untuk Score
+# Score awal
+score = 0
+
+# Menampilkan score
 def show_score(choice, color, font, size):
-    # creating font object score_font
+    # Score Font
     score_font = pygame.font.SysFont(font, size)
 
-    # create the display surface object
-    # score_surface
+    # Render Score Surface (Text)
     score_surface = score_font.render('Score : ' + str(score), True, color)
 
-    # create a rectangular object for the
-    # text surface object
+    # Object segi empat untuk score_surface
     score_rect = score_surface.get_rect()
 
-    # displaying text
+    # Menampilkan text
     game_window.blit(score_surface, score_rect)
 
-
-# game over function
+# Fungsi Game Over
 def game_over():
-    # creating font object my_font
+    # Game over font
     my_font = pygame.font.SysFont('times new roman', 35)
 
-    # creating a text surface on which text
-    # will be drawn
+    # Render Game over surface (Text)
     game_over_surface = my_font.render(
         'Your Score is : ' + str(score), True, red)
 
-    # create a rectangular object for the text
-    # surface object
+    # Object segi empat untuk game over surface object
     game_over_rect = game_over_surface.get_rect()
-    # setting position of the text
+
+    # Letak text
     game_over_rect.midtop = (window_x / 2, window_y / 4)
 
-    # blit will draw the text on screen
+    # Menampilkan text
     game_window.blit(game_over_surface, game_over_rect)
+
+    # Update keseluruhan display
     pygame.display.flip()
 
+    # Check apakah menekan 'space' untuk exit
     while(True):
         event = pygame.event.wait()
         if event.type == KEYDOWN:
@@ -117,32 +116,39 @@ def game_over():
                 quit()
 
 
+
+# Function untuk menggerakan Snake
+# Heuristic Function
 def calculate_distance(snake_x, snake_y):
     return sqrt(pow(snake_x - fruit_position[0], 2) + pow(snake_y - fruit_position[1], 2))
 
-
-def border_check(move):
-    if move == 'UP':
-        return snake_position[1] - 10 > 0
-    if move == 'DOWN':
-        return snake_position[1] + 10 < window_y
-    if move == 'LEFT':
-        return snake_position[0] - 10 > 0
-    if move == 'RIGHT':
-        return snake_position[0] + 10 < window_x
-
-
-def get_next_move_coordinates(move):
-    if move == 'UP':
-        return snake_position[0], snake_position[1] - 10
-    if move == 'DOWN':
-        return snake_position[0], snake_position[1] + 10
-    if move == 'LEFT':
-        return snake_position[0] - 10, snake_position[1]
-    if move == 'RIGHT':
-        return snake_position[0] + 10, snake_position[1]
+# Menggerakan Snake
+def move_snake():
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
 
 
+# Agar input dua tombol bersamaan tidak bertabrakan
+def change_direction(change_to):
+    global direction
+
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+
+# Mendapatkan gerakan selanjutnya yang tidak menghasilkan game over
 def consider_direction(possible_moves):
     lowest = (sys.maxsize, '')
 
@@ -155,6 +161,31 @@ def consider_direction(possible_moves):
     return lowest[1]
 
 
+# Check apakah ada border di depan
+def border_check(move):
+    if move == 'UP':
+        return snake_position[1] - 10 > 0
+    if move == 'DOWN':
+        return snake_position[1] + 10 < window_y
+    if move == 'LEFT':
+        return snake_position[0] - 10 > 0
+    if move == 'RIGHT':
+        return snake_position[0] + 10 < window_x
+
+
+# Mendapat posisi selanjutnya
+def get_next_move_coordinates(move):
+    if move == 'UP':
+        return snake_position[0], snake_position[1] - 10
+    if move == 'DOWN':
+        return snake_position[0], snake_position[1] + 10
+    if move == 'LEFT':
+        return snake_position[0] - 10, snake_position[1]
+    if move == 'RIGHT':
+        return snake_position[0] + 10, snake_position[1]
+
+
+# Apakah di dalam boundary atau tidak
 def is_in_boundary(move, coor):
     if move == 'UP':
         return coor[1] > 0
@@ -166,10 +197,17 @@ def is_in_boundary(move, coor):
         return coor[0] < window_x
 
 
-def consider_180_deg_move(possible_moves, change_to):
-    possible_moves = secondary_move[change_to]  # UP, DOWN
+# Boolean untuk consider_180_deg_move
+def is_move_180_deg(change_to):
+    global direction
 
+    return direction == opposite_direction[change_to]
+
+
+# Mendapatkan gerakan terbaik untuk 180
+def consider_180_deg_move(possible_moves, change_to):
     lowest = (sys.maxsize, '')
+    possible_moves = secondary_move[change_to]  # UP, DOWN
 
     coor = get_next_move_coordinates(possible_moves[0])
     # distance of first possible move
@@ -188,25 +226,7 @@ def consider_180_deg_move(possible_moves, change_to):
     return lowest[1]
 
 
-def change_direction(change_to):
-    global direction
-
-    if change_to == 'UP' and direction != 'DOWN':
-        direction = 'UP'
-    if change_to == 'DOWN' and direction != 'UP':
-        direction = 'DOWN'
-    if change_to == 'LEFT' and direction != 'RIGHT':
-        direction = 'LEFT'
-    if change_to == 'RIGHT' and direction != 'LEFT':
-        direction = 'RIGHT'
-
-
-def is_move_180_deg(change_to):
-    global direction
-
-    return direction == opposite_direction[change_to]
-
-
+# Boolean untuk check_is_eating_body
 def is_eating_body(coor):
     next_move = [coor[0], coor[1]]
     if next_move in snake_body:
@@ -214,6 +234,7 @@ def is_eating_body(coor):
     return False
 
 
+# Melihat apakah akan memakan body
 def check_is_eating_body(possible_moves):
     global direction
 
@@ -227,6 +248,7 @@ def check_is_eating_body(possible_moves):
         check_is_eating_body(possible_moves)
 
 
+# Melihat gerakan terbaik untuk mendapat buah
 def determine_direction(possible_moves, change_to):
     global direction
 
@@ -237,31 +259,19 @@ def determine_direction(possible_moves, change_to):
     check_is_eating_body(possible_moves)
 
 
-def move_snake():
-    if direction == 'UP':
-        snake_position[1] -= 10
-    if direction == 'DOWN':
-        snake_position[1] += 10
-    if direction == 'LEFT':
-        snake_position[0] -= 10
-    if direction == 'RIGHT':
-        snake_position[0] += 10
-
-
 # Main Function
 while True:
-    pygame.event.get()
+    pygame.event.get() # Agar tidak error jika interaksi dengan window
     possible_moves = ['UP', 'DOWN', 'LEFT', 'RIGHT']
 
+    # Mengganti arah selanjutnya
     change_to = consider_direction(possible_moves)
 
     determine_direction(possible_moves, change_to)
 
     move_snake()
 
-    # Snake body growing mechanism
-    # if fruits and snakes collide then scores will be
-    # incremented by 10
+    # Mekanisme gerakan dan penambahan body snake
     snake_body.insert(0, list(snake_position))
     if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
         score += 10
@@ -269,17 +279,19 @@ while True:
     else:
         snake_body.pop()
 
+    # Respawn Fruit
     if not fruit_spawn:
         fruit_position = [random.randrange(1, (window_x // 10)) * 10,
                           random.randrange(1, (window_y // 10)) * 10]
-
     fruit_spawn = True
     game_window.fill(black)
 
+    # Render Snake
     for pos in snake_body:
         pygame.draw.rect(game_window, green, pygame.Rect(
             pos[0], pos[1], 10, 10))
 
+    # Render Fruit
     pygame.draw.rect(game_window, white, pygame.Rect(
         fruit_position[0], fruit_position[1], 10, 10))
 
@@ -294,11 +306,11 @@ while True:
         if snake_position[0] == block[0] and snake_position[1] == block[1]:
             game_over()
 
-    # displaying score countinuously
+    # Menampilkan Score
     show_score(1, white, 'times new roman', 20)
 
-    # Refresh game screen
+    # Refresh layar
     pygame.display.update()
 
-    # Frame Per Second /Refresh Rate
+    # Frame Per Second / Refresh Rate
     fps.tick(snake_speed)
